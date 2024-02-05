0ddc20ec02e424923e10dd31a50608a7cad5e2f0
violaine-drt <viodarrent@gmail.com>
2024-02-05 14:51:14 +0100

    debug de Settings

--- settings.php
+++ settings.php
@@ -58,6 +58,10 @@
                  */
                 $laQuestionEnSql = "
                     SELECT users.*, 
+                    users.alias AS userAlias,
+                    users.id AS userId,
+                    users.email AS userEmail,
+                    users.password AS userPass,
                     count(DISTINCT posts.id) as totalpost, 
                     count(DISTINCT given.post_id) as totalgiven, 
                     count(DISTINCT recieved.user_id) as totalrecieved 
@@ -67,36 +71,38 @@
                     LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
                     WHERE users.id = '$userId' 
                     GROUP BY users.id
+                    LIMIT 100
                     ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 if ( ! $lesInformations)
                 {
                     echo("Échec de la requete : " . $mysqli->error);
                 }
-                $user = $lesInformations->fetch_assoc();
+                while ($user = $lesInformations->fetch_assoc())
+                {
 
                 /**
                  * Etape 4: à vous de jouer
                  */
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>                
+                // echo "<pre>" . print_r($user, 1) . "</pre>";
+                ?>
                 <article class='parameters'>
-                    <h3>Mes paramètres</h3>
+                    <h3>Mes Paramètres</h3>
                     <dl>
                         <dt>Pseudo</dt>
-                        <dd>Félicie</dd>
+                        <dd><?php echo $user['userAlias'] ?></dd>
                         <dt>Email</dt>
-                        <dd>felicie@test.org</dd>
+                        <dd><?php echo $user['userEmail'] ?></dd>
                         <dt>Nombre de message</dt>
-                        <dd>42</dd>
+                        <dd><?php echo $user['totalpost'] ?></dd>
                         <dt>Nombre de "J'aime" donnés </dt>
-                        <dd>12</dd>
+                        <dd><?php echo $user['totalgiven'] ?></dd>
                         <dt>Nombre de "J'aime" reçus</dt>
-                        <dd>53</dd>
+                        <dd><?php echo $user['totalrecieved'] ?></dd>
                     </dl>
-
                 </article>
+                <?php } ?>
             </main>
         </div>
     </body>
